4fe0022dc8a18ee4cd5402af5e357602ef02f75b
boschcarola294 <boschcarola294@gmail.com>
2025-03-16 06:40:28 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,7 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Your code here
+        int num = (int)(Math.random() * 10);
+        System.out.println("Random number: " + num);
+    }
+}
