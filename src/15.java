39efd52a5f6857346537500387b2c6e7d3b3eb7b
boschcarola294 <boschcarola294@gmail.com>
2025-03-22 15:59:13 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,10 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of using loops and conditionals in Java
+        int count = 5;
+        while (count > 0) {
+            System.out.println("This is a loop, the counter is " + count);
+            count--;
+        }
+    }
+}
