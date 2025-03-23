18419816723e497f7b2f8780169a3583f5350b57
boschcarola294 <boschcarola294@gmail.com>
2025-03-23 07:28:22 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,12 @@
+public class SimpleProjectCodeGenerator {
+    public static void main(String[] args) {
+        // Example: Print "Hello World"
+        System.out.println("Hello World");
+        
+        // Example: Add two numbers and print the result
+        int number1 = 5;
+        int number2 = 3;
+        int sum = number1 + number2;
+        System.out.println("Sum is: " + sum);
+    }
+}
