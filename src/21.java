d2c9b2c4a5ff574ba7559071d40d2d36c36dfced
boschcarola294 <boschcarola294@gmail.com>
2025-04-01 15:54:47 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,7 @@
+public class SimpleSchoolProject {
+    // Example method to demonstrate some basic Java concepts
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
