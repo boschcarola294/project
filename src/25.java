7bcf7b29ded3272ab343f22e23bf3986f36bf179
boschcarola294 <boschcarola294@gmail.com>
2025-04-06 00:02:58 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,7 @@
+public class SimpleProject {
+
+    public static void main(String[] args) {
+        // Example code to demonstrate basic Java programming concepts
+        System.out.println("Hello, World!");
+    }
+}
