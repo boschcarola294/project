fb2d040dbbf23d0ae498c799149d4fe0c6e5eb8d
boschcarola294 <boschcarola294@gmail.com>
2025-04-11 12:59:34 +0000

    Add 47.java

---
+++ src/47.java
@@ -0,0 +1,9 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of using a loop to iterate over elements in an array
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i : numbers) {
+            System.out.println(i);
+        }
+    }
+}
