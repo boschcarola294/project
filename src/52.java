81d6cc902aaf86d8630d5f1e0004aac4d146110f
boschcarola294 <boschcarola294@gmail.com>
2025-04-11 14:11:39 +0000

    Add 52.java

---
+++ src/52.java
@@ -0,0 +1,11 @@
+public class SimpleSchoolProject {
+
+    public static void main(String[] args) {
+        // Example of basic Java program
+
+        System.out.println("Hello, World!");
+        
+        // You can add more statements and output here
+        
+    }
+}
