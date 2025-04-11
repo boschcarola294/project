80ff01fa1bb98ca9e97c70c676e11e49f88f755f
boschcarola294 <boschcarola294@gmail.com>
2025-04-11 13:00:55 +0000

    Add 48.java

---
+++ src/48.java
@@ -0,0 +1,14 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example Java code for a simple school project
+
+        System.out.println("Hello World!");
+        
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+    }
+}
