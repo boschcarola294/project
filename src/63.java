6c020e32e574d35858edb5ecacfcde88bad2fff4
boschcarola294 <boschcarola294@gmail.com>
2025-05-19 08:05:50 +0000

    Add 63.java

---
+++ src/63.java
@@ -0,0 +1,8 @@
+public class SimpleProject {
+    public static void main(String[] args) {
+        // Example of a simple loop that prints numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number " + i);
+        }
+    }
+}
