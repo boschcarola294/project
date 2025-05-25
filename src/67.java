be1f68aee166dc6cf2726f991a24d4cf41065e9b
boschcarola294 <boschcarola294@gmail.com>
2025-05-25 03:41:24 +0000

    Add 67.java

---
+++ src/67.java
@@ -0,0 +1,6 @@
+public class SimpleProject {
+    public static void main(String[] args) {
+        System.out.println("This is the first line of the Java code.");
+        System.out.println("This is a simple school project.");
+    }
+}
